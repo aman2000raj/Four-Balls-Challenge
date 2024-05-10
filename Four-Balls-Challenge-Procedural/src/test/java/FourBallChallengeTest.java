2a8f1f4908f3fb098c22cb83db0db71d257b8291
aman2000raj <amankayshap10@gmail.com>
2024-05-10 19:45:23 +0530

    Add Procedural attempt.

---
+++ Four-Balls-Challenge-Procedural/src/test/java/FourBallChallengeTest.java
@@ -0,0 +1,10 @@
+import processing.core.PApplet;
+
+public class FourBallChallengeTest {
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge", args);
+
+        FourBallChallenge fourBallChallenge = new FourBallChallenge();
+
+    }
+}
